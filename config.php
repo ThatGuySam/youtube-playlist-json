8da8e0d1a5784cc4111da11fa76e29c3b0e3d9cd
Sam Carlton <thatguy@samuelcarlton.com>
2015-06-29 20:12:55 -0500

    Added Youtube Config Vars

--- config.php
+++ config.php
@@ -11,45 +11,18 @@
 	$debug = $request;
 	
 	$config_vars = array(
-	    "TWITTER_CONSUMER_KEY"		=> 0,
-	    "TWITTER_CONSUMER_SECRET"	=> 0,
-	    "OAUTH_TOKEN"				=> 0,
-	    "OAUTH_SECRET"				=> 0,
-	    "POSTS_COUNT"				=> 20,
-	    "TWITTER_USER"				=> 'ThatGuySam',
-	    "TWITTER_SLUG"				=> 'periscope-stream',
-	    "API_KIND"					=> 'search/tweets',
-	    "QUERY"						=> 'q=#nature',
-	    "RESULT_TYPE"				=> 'recent',
-	    "CACHE_TIME"				=> 10
+	    "YOUTUBE_API_KEY"			=> 0,
+	    "PLAYLIST_ID"				=> $id,
+	    "CACHE_TIME"				=> 1//3600,//One Hour
 	);
 	
 	//Dev Values
 	if(in_array($_SERVER['REMOTE_ADDR'], $whitelist)) include('config-dev.php');
 	
 	
-	//Steup Config Vars as Constants
+	//Setup Config Vars as Constants
 	foreach( $config_vars as $key => $default ){
-		
 		$val = getenv($key);
-		
 		if( empty( $val ) ) $val = $default;
-		
 		define($key, $val);
-		
 	}
-	
-/*
-	// The OAuth credentials you received when registering your app at Twitter
-	define("TWITTER_CONSUMER_KEY", getenv('TWITTER_CONSUMER_KEY'));
-	define("TWITTER_CONSUMER_SECRET", getenv('TWITTER_CONSUMER_SECRET'));
-	
-	// The OAuth data for the twitter account
-	define("OAUTH_TOKEN", getenv('TWITTER_USER'));
-	define("OAUTH_SECRET", getenv('TWITTER_USER'));
-	
-	
-	define("POSTS_COUNT", getenv('POSTS_COUNT'));
-*/
-	
-	//"ThatGuySam"
